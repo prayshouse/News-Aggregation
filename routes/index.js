6a14207d211240b0060a1493caeba9c684e5787a
prayshouse <3140103730@zju.edu.cn>
2017-05-29 12:32:09 +0800

    modify the ways router using

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(app) {
+    app.get('/', function (req, res, next) {
+        res.render('index', { title: 'Express' });
+    });
+    
+    app.get('/login', function (req, res, next) {
+        res.send('<h1>log in<\h1>');
+    });
+}
 
-module.exports = router;
+/* GET home page. */
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+
+//module.exports = router;
